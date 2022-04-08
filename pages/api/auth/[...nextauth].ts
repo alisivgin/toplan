7ef491baddf44032ac57b7b5247084283a1d7c71
Ali Sivgin <ali.sivgin@getir.com>
2022-04-08 12:55:28 +0300

    schema update

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,8 @@
 import NextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
+import { prisma } from 'lib/prisma';
 
-const prisma = new PrismaClient();
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
--- pages/app/signin.tsx
+++ pages/app/signin.tsx
@@ -2,24 +2,21 @@ import { GetServerSidePropsContext } from 'next';
 import { getSession } from 'next-auth/react';
 import SignInForm from 'containers/SignInFom';
 
-export function SignIn() {
+export default function SignIn() {
   return <SignInForm />;
 }
 
 export async function getServerSideProps({ req }: GetServerSidePropsContext) {
   const session = await getSession({ req });
   console.log(session);
-  // if (session) {
-  //   return {
-  //     redirect: {
-  //       permanent: true,
-  //       destination: '/app/1235',
-  //     },
-  //   };
-  // }
+  if (session) {
+    return {
+      redirect: {
+        destination: '/app/client/xyz',
+      },
+    };
+  }
   return {
     props: {},
   };
 }
-
-export default SignIn;
